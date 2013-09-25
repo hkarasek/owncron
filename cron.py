9aee77a0fe11c5c48da39faa353ac79bcfe15daa
Jan Karásek <hkar@hkar.eu>
2013-09-25 22:45:38 +0200

    added: add, remove, list

--- cron.py
+++ cron.py
@@ -1,15 +1,36 @@
-import time
-import sqlite3
-import argparse
-import requests
+import time, sqlite3, argparse, requests
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--links", help="show links from database",
-                    action="store_true")
+parser.add_argument("--links", help="show links from database", action="store_true")
+parser.add_argument("--add", help="add link to db", action="store_true")
+parser.add_argument("--remove", help="remove link from db", action="store_true")
+
 args = parser.parse_args()
 
 conn = sqlite3.connect('sites.db')
 c = conn.cursor()
+
+if args.remove:
+	print('remove link from db: \n ###############################')
+	print('links:')
+	for row in c.execute('SELECT * FROM sites'):
+		print(row[0],"\t",row[1],"\t",row[2])
+	print('###############################')
+	url = input('url: \t\t')
+	period = input('period: \t')
+	c.execute("DELETE FROM sites WHERE url = \'"+url+"\' AND period = \'"+period+"\'")
+	conn.commit()
+	conn.close()
+	exit()
+
+if args.add:
+	print('Add link to db: \n###############################')
+	url = input('url: \t\t')
+	period = input('period: \t')
+	c.execute("INSERT INTO sites VALUES (?,?,?)", (url, period,-1))
+	conn.commit()
+	conn.close()
+	exit()
 
 if args.links:
 	for row in c.execute('SELECT * FROM sites'):
