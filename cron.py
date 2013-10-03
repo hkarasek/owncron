a0171158cefed6942fa2ba575edbf698b1164063
Jan Karásek <hkar@hkar.eu>
2013-10-03 15:50:40 +0200

    add except

--- cron.py
+++ cron.py
@@ -41,14 +41,14 @@ if args.list:
 while 1:
 	for row in c.execute('SELECT * FROM sites'):
 		if int((time.time() - int(row[2]))) > int(row[1]):
-			r = requests.get(row[0])
-			if r:
+			try:
+				r = requests.get(row[0])
 				print(row[0], row[1], row[2], "\t -- ok")
-			else:
+				c.execute("UPDATE sites SET last=? WHERE url=?", (time.time(), row[0]))        
+				conn.commit()
+				del r
+			except:
 				print(row[0], row[1], row[2], "\t -- no")
-			del r
-			c.execute("UPDATE sites SET last=? WHERE url=?", (time.time(), row[0]))        
-			conn.commit()
-	time.sleep(30)
+	time.sleep(10)
 
 conn.close()
